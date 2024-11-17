ddfb888c2b898dc3e98290b109138b5490cf471e
mgichev <mishagichev@yandex.ru>
2024-11-17 14:58:30 +0300

    Initial commit

---
+++ src/Child.java
@@ -0,0 +1,28 @@
+public class Child extends Parent {
+    static {
+        System.out.println("Child:static 1");
+    }
+
+    {
+        System.out.println("Child:instance 1");
+    }
+
+    static {
+        System.out.println("Child:static 2");
+    }
+
+    public Child() {
+        super();
+        System.out.println("Child:constructor");
+    }
+
+    public Child(String name) {
+        super(name);
+        System.out.println("Child:name-constructor");
+        //System.out.printf("Child:%s-constructor", name);
+    }
+
+    {
+        System.out.println("Child:instance 2");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Child childWithoutName = new Child();
+        Child childIvan = new Child("Ivan");
+    }
+}
