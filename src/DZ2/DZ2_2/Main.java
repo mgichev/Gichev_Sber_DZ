b52631768c93238f30193b217549d6439959f571
mgichev <mishagichev@yandex.ru>
2024-11-23 12:12:40 +0300

    Добавлено ДЗ 2.2

--- src/DZ2/DZ2_2/Main.java
+++ src/DZ2/DZ2_2/Main.java
@@ -1,11 +1,13 @@
 package DZ2.DZ2_2;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         TelephoneBook telephoneBook = new TelephoneBook();
 
         telephoneBook.add("John", "12345");
-        telephoneBook.add("John", "5789");
+        telephoneBook.add("John", List.of("567", "293"));
 
         var record = telephoneBook.get("John");
         System.out.println(record);
